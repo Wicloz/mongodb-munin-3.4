2f2a4ce469e0488de12259463e58110ebdb06d3e
Wilco de Boer <wfh@wilcodeboer.me>
2017-08-24 12:42:13 +0200

    Merge mongodb command scripts

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
 install:
-	mkdir -p /usr/lib/mongostatus
-	touch /usr/lib/mongostatus/__init__.py
-	touch /usr/lib/mongostatus/init.py
-	install -m 755 ./mongostatus/mongodb_server_status.py /usr/lib/mongostatus/mongodb_server_status.py
-	install -m 755 ./mongostatus/mongodb_list_databases.py /usr/lib/mongostatus/mongodb_list_databases.py
+	mkdir -p /usr/lib/mongopython
+	touch /usr/lib/mongopython/__init__.py
+	touch /usr/lib/mongopython/init.py
+	install -m 755 ./mongopython/mongodb_commands.py /usr/lib/mongopython/mongodb_commands.py
 	install -m 755 ./plugins/* /usr/share/munin/plugins/
 
 enable:
 	munin-node-configure --shell --families contrib | grep 'mongodb_' | sh
 
 clean:
-	rm -rf /usr/lib/mongostatus
 	rm -f /etc/munin/plugins/mongodb_*
 	rm -f /usr/share/munin/plugins/mongodb_*
+	rm -rf /usr/lib/mongopython
---
+++ mongopython/mongodb_commands.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+from os import environ
+import pymongo
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urllib as urlparse
+
+url = ''
+if 'mongourl' in environ:
+    url = environ['mongourl']
+
+if url == '':
+    socket = ''
+    if 'mongosocket' in environ:
+        socket = urlparse.quote_plus(environ['mongosocket'])
+
+    host = '127.0.0.1'
+    if 'mongohost' in environ:
+        host = environ['mongohost']
+
+    port = 27017
+    if 'mongoport' in environ:
+        port = int(environ['mongoport'])
+
+    user = ''
+    if 'mongouser' in environ:
+        user = urlparse.quote_plus(environ['mongouser'])
+
+    password = ''
+    if 'mongopassword' in environ:
+        password = urlparse.quote_plus(environ['mongopassword'])
+
+    authsource = 'admin'
+    if 'mongoauthsource' in environ:
+        authsource = environ['mongoauthsource']
+
+    if password == '' and user == '':
+        urlstart = 'mongodb://'
+        urlend = ''
+    else:
+        urlstart = 'mongodb://%s:%s@' % (user, password)
+        urlend = '/%s' % (authsource)
+
+    if socket != '':
+        url = urlstart + socket + urlend
+    else:
+        url = urlstart + host + ":" + str(port) + urlend
+
+def getServerStatus(conn_timeout_secs = 30):
+    client = pymongo.MongoClient(url, serverSelectionTimeoutMS=(conn_timeout_secs * 1000))
+    return client.admin.command('serverStatus', rangeDeleter=True, repl=True)
+
+def getListDatabases(conn_timeout_secs = 30):
+    client = pymongo.MongoClient(url, serverSelectionTimeoutMS=(conn_timeout_secs * 1000))
+    return client.admin.command('listDatabases')
